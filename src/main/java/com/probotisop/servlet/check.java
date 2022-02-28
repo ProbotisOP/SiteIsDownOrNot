5ae3580f8e8066ddbb5f2ee93b46733bca50c34d
ProbotisOP <39401172+ProbotisOP@users.noreply.github.com>
2022-02-28 15:48:30 +0530

    Files Added

---
+++ src/main/java/com/probotisop/servlet/check.java
@@ -0,0 +1,54 @@
+package com.probotisop.servlet;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/check")
+public class check extends HttpServlet {
+
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		String url = request.getParameter("site");
+
+		URL u = new URL(url);
+
+		try {
+			HttpURLConnection connection = (HttpURLConnection) u.openConnection();
+			connection.setConnectTimeout(3000);
+			connection.connect();
+
+			if (connection.getResponseCode() == 200) {
+				System.out.println("site is UP");
+				String status = "SITE IS UP";
+				request.setAttribute("status", status);
+				RequestDispatcher rd = request.getRequestDispatcher("result.jsp");
+				rd.forward(request, response);
+
+			}
+
+			else {
+				System.out.println("site is DOWN");
+				request.setAttribute("status", "SITE IS UP !!");
+				RequestDispatcher rd = request.getRequestDispatcher("result.jsp");
+				rd.forward(request, response);
+			}
+		}
+
+		catch (Exception e) {
+			System.out.println("TIMEOUT");
+			request.setAttribute("status", "TIMEOUT , SEEMS DOWN");
+			RequestDispatcher rd = request.getRequestDispatcher("result.jsp");
+			rd.forward(request, response);
+
+		}
+
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<html>
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+	crossorigin="anonymous">
+<body>
+
+
+
+	<br>
+	<h2 style="text-align: center;">IS DOWN OR NOT ?</h2>
+
+	<br>
+	<br>
+	<center>
+		<form action="check" method="post">
+			SITE : <input type="text" name="site"> <input type="submit"
+				value="submit">
+		</form>
+
+		<br>
+
+	</center>
+
+</body>
+</html>
+
+
